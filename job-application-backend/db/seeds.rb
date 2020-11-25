db896148757f3d6b2baadc053e7913f241d8b11a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-25 12:19:36 -0800

    Backend DB

--- job-application-backend/db/seeds.rb
+++ job-application-backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Job.create(title: "Software Engineer", employer: "Google", location: "Silicon Valley, CA", description: "Develop software apps", release_date: "11/25/20", job_type: "Full-Time")
+Job.create(title: "Network Engineer", employer: "Cisco", location: "Houston, TX", description: "Maintain network security", release_date: "10/13/20", job_type: "Full-Time")
+Job.create(title: "System Architect", employer: "Microsoft", location: "New York, NY", description: "Computer Programming", release_date: "8/14/20", job_type: "Full-Time")
+
