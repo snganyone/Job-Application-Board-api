71c14b839ae7a2e725bb017638e254e35c239a74
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-25 17:37:27 -0800

    Javascript Frontend

--- job-application-frontend/src/api_service.js
+++ job-application-frontend/src/api_service.js
@@ -3,13 +3,11 @@ class API{
         this.base_url = base_url;
     }
 
+    //Fetch Request to Rails API
+
     GetJobs = () => fetch(`${this.base_url}`).then(response => response.json());
 
-    RenderJobs = (jobs) => {
-        const table = document.getElementById("table-body");
-        table.innerHTML = "";
-        jobs.forEach((element) => table.innerHTML += jobHTML(element));
-    }
+    //Render JSON Data in table
 
     JobHTML = (job) => {
         return `
@@ -26,6 +24,15 @@ class API{
         </tr>
     </tbody>`;
     }
+
+    //Parse through JSON Data
+    
+    RenderJobs = (jobs) => {
+        const table = document.getElementById("table-body");
+        table.innerHTML = "";
+        jobs.forEach((element) => table.innerHTML += jobHTML(element));
+    }
+
 }
 
 
