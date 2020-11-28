eac345874717cceb28390529ed5933b24ccc41fb
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-27 22:56:10 -0800

    Create Job

--- job-application-backend/app/controllers/jobs_controller.rb
+++ job-application-backend/app/controllers/jobs_controller.rb
@@ -3,4 +3,14 @@ class JobsController < ApplicationController
         @jobs = Job.all
         render json: @jobs, except: [:created_at, :updated_at]
     end
+
+    def create
+        @job = Job.new()
+    end
+
+    private
+
+    def job_params
+		params.require(:job).permit(:title, :employer, :location, :description, :release_date)
+    end
 end
--- job-application-frontend/src/api_service.js
+++ job-application-frontend/src/api_service.js
@@ -109,7 +109,8 @@ class API{
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
-            }
+            },
+            body: JSON.stringify(data),
         };
         fetch(this.base_url, config);
     }
