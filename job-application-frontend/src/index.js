372cb594e33aec88c656ad5f0748d673a20595e4
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-02 19:28:47 -0800

    Show/Hide Job Form

--- job-application-frontend/src/index.js
+++ job-application-frontend/src/index.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const api_service = new API("http://localhost:3000/jobs");
-    const agency_api_service = new API("http://localhost:3000/agencies");
-    
     api_service.GetJobs(); 
 
-    console.log(agency_api_service.GetJobs());
+    const agency_api_service = new API("http://localhost:3000/agencies");
     agency_api_service.AgencyDropdownForm();
 
     const job = new Job();
@@ -16,27 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
     job.DeleteJobListener();
 
     const utility = new Utility();
+    console.log(utility);
+    utility.DisplayForm();
 
     
-    let button = document.getElementById("new-job");
-    let job_form = document.getElementById("new-job-form"); 
+    // let button = document.getElementById("new-job");
+    // let job_form = document.getElementById("new-job-form"); 
 
-    function show(){
-        job_form.style.visibility = "visible";
-    }
-    function hide(){
-        job_form.style.visibility = "hidden";
-    }
+    // function show(){
+    //     job_form.style.visibility = "visible";
+    // }
+    // function hide(){
+    //     job_form.style.visibility = "hidden";
+    // }
 
-    function toggle(){
-        if(job_form.style.visibility === "hidden"){
-            show();
-        } else{
-            hide();
-        }
-    }
-    hide();
+    // function toggle(){
+    //     if(job_form.style.visibility === "hidden"){
+    //         show();
+    //     } else{
+    //         hide();
+    //     }
+    // }
+    // hide();
 
-    button.addEventListener("click", toggle, false);
+    // button.addEventListener("click", toggle, false);
 });
 
